27639b88774a5654ea50230c8d7fb9ce9ae45599
alfredochs <chanta.alfredo@gmail.com>
2021-12-21 22:05:04 +0100

    cards collegate

--- resources/views/comics/details.blade.php
+++ resources/views/comics/details.blade.php
@@ -2,7 +2,23 @@
 
 @section('titolo_pagina' , 'Dettagli')
 @section('content')
-<h1> {{$comic['title']}}</h1>
+<div class="container align-items-center my-3 w-50 d-flex ">
+    <div class="">
+        <img src="{{$comic['thumb']}}" alt="">
+    </div>
+    <div class="ps-4">
+        <h5>{{$comic['title'] }}</h5>
+        <div>
+            <span><strong>Dettagli:</strong></span>
+            <ul class="list-unstyled">
+                <li>{{$comic['price']}}</li>
+                <li>{{$comic['sale_date']}}</li>
+                <li>{{$comic['type']}}</li>
+            </ul>
+        </div>
+        <p>{{$comic['description']}}</p>
+    </div>
+</div>
     
 @endsection
     
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,8 +7,10 @@
 @section('content')
 <div class="container-fluid bg-dark">
     <div class="container m-auto row row-cols-1 row-cols-md-6 g-4 grid-cards">
-        @foreach($comics  as $comic)
-        <a href="{{route('comics.details',['id' => 3])}}" class="d-flex flex-column text-decoration-none text-white">
+        @foreach($comics  as $indice => $comic)
+        {{-- metodo con indice preso dall foreach --}}
+        {{-- <a href="{{route('comics.details',['id' => $indice])}}" class="d-flex flex-column text-decoration-none text-white"> --}}
+        <a href="{{route('comics.details',['id' => $loop ->index])}}" class="d-flex flex-column text-decoration-none text-white">
             <div>
                 <img src="{{$comic['thumb']}}" class="card-img-top my-img" alt="...">
             </div>
